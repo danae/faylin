b897fc795702605ec446a3510ec1b23713a9fbde
Danae Dekker <info@danaedekker.com>
2021-07-29 23:01:48 +0200

    Chenged route middleware to individual routes

--- app/routes.php
+++ app/routes.php
@@ -18,7 +18,7 @@ return function(App $app)
   // Backend controller routes
   $app->group('/api/v1', function(RouteCollectorProxy $group)
   {
-    // Return a JWT token for an authorized client
+    // Return a JWT token for an authenticated client
     $group->post('/token', [BackendController::class, 'token'])
       ->setName('token');
 
@@ -28,25 +28,29 @@ return function(App $app)
       ->setName('capabilities');
 
     // Image controller routes
-    $images = $group->group('/images', function(RouteCollectorProxy $group)
+    $group->group('/images', function(RouteCollectorProxy $group)
     {
       // Return all images as a JSON response
       $group->get('/', [ImageController::class, 'index'])
+        ->add(AuthorizationMiddleware::class)
         ->setName('images.index');
 
       // Get an image as a JSON response
       $group->get('/{id:[A-Za-z0-9-_]+}', [ImageController::class, 'get'])
         ->add(ImageResolverMiddleware::class)
+        ->add(AuthorizationMiddleware::class)
         ->setName('images.get');
 
       // Patch an image and return the image as a JSON response
       $group->patch('/{id:[A-Za-z0-9-_]+}', [ImageController::class, 'patch'])
         ->add(ImageResolverMiddleware::class)
+        ->add(AuthorizationMiddleware::class)
         ->setName('images.patch');
 
       // Delete an image
       $group->delete('/{id:[A-Za-z0-9-_]+}', [ImageController::class, 'delete'])
         ->add(ImageResolverMiddleware::class)
+        ->add(AuthorizationMiddleware::class)
         ->setName('images.delete');
 
       // Download the contents of an image
@@ -56,51 +60,56 @@ return function(App $app)
 
       // Upload an image
       $group->post('/upload', [ImageController::class, 'upload'])
+        ->add(AuthorizationMiddleware::class)
         ->setName('images.upload');
 
       // Replace an image
       $group->post('/{id:[A-Za-z0-9-_]+}/upload', [ImageController::class, 'replace'])
         ->add(ImageResolverMiddleware::class)
+        ->add(AuthorizationMiddleware::class)
         ->setName('images.replace');
     });
 
-    $images->add(AuthorizationMiddleware::class);
-
     // User controller routes
-    $users = $group->group('/users', function(RouteCollectorProxy $group)
+    $group->group('/users', function(RouteCollectorProxy $group)
     {
       // Return all users as a JSON response
       $group->get('/', [UserController::class, 'index'])
+        ->add(AuthorizationMiddleware::class)
         ->setName('users.index');
 
       // Get the authorized user as a JSON response
       $group->get('/me', [UserController::class, 'getMe'])
+        ->add(AuthorizationMiddleware::class)
         ->setName('users.get.me');
 
       // Get a user as a JSON response
       $group->get('/{id:[A-Za-z0-9-_]+}', [UserController::class, 'get'])
         ->add(UserResolverMiddleware::class)
+        ->add(AuthorizationMiddleware::class)
         ->setName('users.get');
 
       // Patch the authorized user and return the user as a JSON response
       $group->patch('/me', [UserController::class, 'patchMe'])
+        ->add(AuthorizationMiddleware::class)
         ->setName('users.patch.me');
 
       // Patch a user and return the user as a JSON response
       $group->patch('/{id:[A-Za-z0-9-_]+}', [UserController::class, 'patch'])
         ->add(UserResolverMiddleware::class)
+        ->add(AuthorizationMiddleware::class)
         ->setName('users.patch');
 
       // Return all images owned by the authorized user as a JSON response
       $group->get('/me/images', [UserController::class, 'imagesMe'])
+        ->add(AuthorizationMiddleware::class)
         ->setName('users.me.images');
 
       // Return all images owned by a user as a JSON response
       $group->get('/{id:[A-Za-z0-9-_]+}/images', [UserController::class, 'images'])
         ->add(UserResolverMiddleware::class)
+        ->add(AuthorizationMiddleware::class)
         ->setName('users.get.images');
     });
-
-    $users->add(AuthorizationMiddleware::class);
   });
 };
