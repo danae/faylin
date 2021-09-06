cecacc83712a002ed20d099cccb02c9c5f60ea8a
Danae Dekker <info@danaedekker.com>
2021-09-06 17:20:32 +0200

    Restyled thumbnails and user thumbnail

--- public/src/components/UserThumbnail.js
+++ public/src/components/UserThumbnail.js
@@ -26,18 +26,39 @@ export default {
   template: `
     <div class="user-thumbnail content">
       <template v-if="user">
-        <div class="box is-primary">
-          <router-link :to="{name: 'user', params: {userId: user.id}}">
-            <h4 class="user-thumbnail-name mb-0">{{ user.title }}</h4>
-          </router-link>
+        <div class="box is-panel my-2">
+          <div class="columns">
+            <div class="column is-4">
+              <div class="media is-align-items-center mb-4">
+                <template v-if="user.avatarUrl">
+                  <div class="media-left mr-2">
+                    <router-link :to="{name: 'user', params: {userId: user.id}}">
+                      <b-image class="avatar is-48x48" :src="user.avatarUrl" :alt="user.title"></b-image>
+                    </router-link>
+                  </div>
+                </template>
 
-          <template v-if="userImages">
-            <div class="columns mt-4">
-              <div class="column is-one-third" v-for="image in userImages" :key="image.id">
-                <image-thumbnail :image="image"></image-thumbnail>
+                <div class="media-content">
+                  <p class="is-size-6 mb-0">
+                    <router-link :to="{name: 'user', params: {userId: user.id}}">{{ user.title }}</router-link>
+                  </p>
+                  <p class="is-size-7 mb-0">
+                    @{{ user.name }}
+                  </p>
+                </div>
               </div>
+
+              <template v-if="user.description">
+                <p class="is-size-7">{{ user.description }}</p>
+              </template>
             </div>
-          </template>
+
+            <div class="column is-8">
+              <template v-if="userImages">
+                <image-thumbnail-list :images="userImages" :fixed="true"></image-thumbnail-list>
+              </template>
+            </div>
+          </div>
         </div>
       </template>
 
